05b2491dc69cc69ddeb29d36819721128902953a
jl-git7 <jonathanlewis.ny@gmail.com>
2026-06-01 17:22:03 -0400

    Fix API routes in standalone mode

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"],
+    },
+  },
 };
 export default nextConfig;
